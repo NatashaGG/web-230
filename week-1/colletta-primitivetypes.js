2febd344c7cac855a1c3556301f5e1ed871c7be6
Natasha C <natashacolletta@gmail.com>
2017-12-03 18:42:01 -0600

    Created Assignment 1.4

---
+++ week-1/colletta-primitivetypes.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Natasha";
+var lastName = "Colletta";
+var age = "24";
+var street = "1234 S Ave";
+var city = "Bellevue";
+var state = "Nebraska";
+var zipCode = "68123";
+
+// output 
+console.log('\n'); 
+
+
+// end program 
