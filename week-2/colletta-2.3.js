34bf2c9231457497912e3b944f18167d0b965442
Natasha Colletta <ncolletta@bellevue.edu>
2017-12-06 08:59:22 -0600

    created headerMsg, outputMsg

--- week-2/colletta-2.3.js
+++ week-2/colletta-2.3.js
@@ -1,5 +1,15 @@
-var header = require('../header.js'); 
+/*
+==========================================
+Title: Exercise 2.3
+Author: Professor Krasso
+Date: December 6, 2017
+Modified By: Natasha Colletta
+Description: Function property to return header and hello message.
+======================================
+*/
 
+var header = require('../header.js'); 
+var headerMsg = header.display("Natasha", "Colletta", "Exercise 2.3");
 // start program
 
 /*
@@ -16,14 +26,16 @@ var header = require('../header.js');
 */ 
 
 // function properties go here...
-
+myName.natashaColletta = "Natasha Colletta";
 
 // function 
 function myName() {
-    
+    return myName.natashaColletta;
 }
 
-// output 
+// output
+var outputMsg = headerMsg + '\n' + '\n' + "Hello " + myName() + "!";
+console.log(outputMsg);
 console.log('\n'); 
 
 
