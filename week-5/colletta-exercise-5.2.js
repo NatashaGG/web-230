698928ea5abf418c2bfe289f2f888e18c9b2dd72
Natasha C <natashacolletta@gmail.com>
2018-01-14 18:58:34 -0600

    understood forEach and redid code

--- week-5/colletta-exercise-5.2.js
+++ week-5/colletta-exercise-5.2.js
@@ -33,15 +33,12 @@ console.log(headerMsg + "\n");
 var favFoods = ["Oysters", "Shrimp", "Steak", "Tacos", "Sushi"];
 
 // Using the forEach() method, iterate over the array
-function getFood() {
-    for (var f =0; f < favFoods.length; f++) {
-        console.log(favFoods[f]); // Call the getFood function using the one-dimensional array created in step one
-    }
-}
+favFoods.forEach(function(food) {
+    console.log(food)
+});
 
 // Output from the getFood() function
 // Output the results
-getFood(favFoods);
 
 
 
